ba3bee871bbc8e4ee0f6f729ee0cf1883c450f60
kraghen <kraghen2007@gmail.com>
2022-05-19 21:47:52 +0200

    0.0.1

--- Scr/Levels/Level.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-export(float) var gravity = 2000
-
-func _ready():
-	Global.current_manager = self
-	
-func _exit_tree():
-	Global.current_manager = null
---
+++ Src/Game Objects/Weapon/Weapon.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+export(PackedScene) var bullet_type
+
+var can_shoot = true
+
+var random_ness = .1
+
+func action(dir):
+	if can_shoot:
+		if $Timer.is_stopped():
+			$Timer.start()
+		can_shoot = false
+		for i in range(10):
+			var inst = Global.instance_create_position(bullet_type,
+							get_parent().get_parent().get_parent(),
+							global_position)
+							
+			inst.dir = (dir+Vector2(rand_range(-random_ness, random_ness), rand_range(-random_ness, random_ness))).normalized()
+			inst.shooter = owner
+			inst = null
+			
+		return true
+	
+	return false
+
+
+
+func _on_Timer_timeout():
+	can_shoot = true
